4116a0fcd7988148701f1af7d0c5cc7037a6e8ea
kaptast <kaptas.tamas@gmail.com>
2022-02-27 23:12:55 +0100

    display music player in vue app

--- music-player-ui/src/App.vue
+++ music-player-ui/src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MusicPlayer from "./components/MusicPlayer.vue";
+</script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div class="flex h-screen w-screen items-center justify-center">
+    <MusicPlayer />
+  </div>
 </template>
