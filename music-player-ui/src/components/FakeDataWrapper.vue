11774f1553ffcd9a61dea9168d9b5a48b202560f
kaptast <kaptas.tamas@gmail.com>
2022-03-02 22:00:43 +0100

    generate fake data

--- music-player-ui/src/components/FakeDataWrapper.vue
+++ music-player-ui/src/components/FakeDataWrapper.vue
@@ -1,15 +1,49 @@
 <template>
-  <MusicPlayer v-bind="data" />
+  <MusicPlayer v-bind="data" @next="next" @previous="previous" />
 </template>
 
 <script setup lang="ts">
 import { reactive } from "vue";
+import faker from "@faker-js/faker";
 import MusicPlayer from "./MusicPlayer.vue";
 
 const data = reactive({
-  artist: "Sample artist",
-  songName: "Sample song",
-  totalTime: 213,
-  currentTime: 134,
+  artist: "",
+  songName: "",
+  totalTime: 0,
+  currentTime: 0,
 });
+
+generateRandomSongData();
+
+setInterval(playSong, 1000);
+
+function next(): void {
+  generateRandomSongData();
+}
+
+function previous(): void {
+  generateRandomSongData();
+}
+
+function playSong(): void {
+  if (data.currentTime >= data.totalTime) {
+    return next();
+  }
+
+  data.currentTime += 1;
+}
+
+function generateRandomSongData(): void {
+  data.artist = faker.random.words(getRandomInt(1, 5));
+  data.songName = faker.random.words(getRandomInt(1, 7));
+  data.totalTime = getRandomInt(60, 300);
+  data.currentTime = 0;
+}
+
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 </script>
