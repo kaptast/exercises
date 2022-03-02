26b9835585e43f5be5dc1265cf06282ae50007de
kaptast <kaptas.tamas@gmail.com>
2022-03-02 22:23:47 +0100

    correctly align current position

--- music-player-ui/src/components/Timeline/TimelineBar.vue
+++ music-player-ui/src/components/Timeline/TimelineBar.vue
@@ -2,7 +2,7 @@
   <div class="h-1 w-full rounded-full bg-black bg-opacity-50">
     <div class="relative h-full rounded-full bg-black" :style="width">
       <div
-        class="absolute right-0 top-[-0.50rem] h-5 w-5 rounded-full bg-black"
+        class="absolute right-[-10px] top-[-0.50rem] h-5 w-5 rounded-full bg-black"
       ></div>
     </div>
   </div>
