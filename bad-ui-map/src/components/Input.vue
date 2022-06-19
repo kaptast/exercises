96990e746e39f5b9f955f6dc39a4424ba66dddb0
kaptast <kaptas.tamas@gmail.com>
2022-06-19 14:48:21 +0200

    adjust input layout

--- bad-ui-map/src/components/Input.vue
+++ bad-ui-map/src/components/Input.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="md:absolute o z-50 w-full lg:px-[40%] md:px-[20%] top-5">
-    <div
-      class="bg-slate-500/40 md:rounded-lg flex items-center space-y-1 md:space-y-3 flex-col p-2"
+  <div
+    class="md:absolute md:z-50 w-full md:w-[80%] lg:w-[40%] md:left-1/2 md:-translate-x-1/2 md:top-5 bg-slate-500/40 md:rounded-lg flex items-center space-y-1 md:space-y-3 flex-col p-2"
+  >
+    <h1 class="text-2xl">Driving</h1>
+    <p class="text-center">
+      Type "update" to update your current location on the map
+    </p>
+    <input type="text" class="rounded-md px-1 w-full" v-model="inputText" />
+    <button
+      type="button"
+      class="bg-slate-300 rounded-md py-2 px-6"
+      @click="tryToUpdateLocation()"
     >
-      <h1 class="text-2xl">Driving</h1>
-      <p class="text-center">
-        Type "update" to update your current location on the map
-      </p>
-      <input type="text" class="rounded-md px-1 w-full" v-model="inputText" />
-      <button
-        type="button"
-        class="bg-slate-300 rounded-md py-2 px-6"
-        @click="tryToUpdateLocation()"
-      >
-        Send
-      </button>
-    </div>
+      Send
+    </button>
   </div>
 </template>
 
