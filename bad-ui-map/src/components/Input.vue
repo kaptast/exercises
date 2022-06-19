eb188ae93363923bb4fc42f22dad73637cf1b46e
kaptast <kaptas.tamas@gmail.com>
2022-06-19 17:47:42 +0200

    use form instead of click event handler

--- bad-ui-map/src/components/Input.vue
+++ bad-ui-map/src/components/Input.vue
@@ -1,5 +1,6 @@
 <template>
-  <div
+  <form
+    @submit.prevent="tryToUpdateLocation()"
     class="md:absolute md:z-50 w-full md:w-[80%] lg:w-[40%] md:left-1/2 md:-translate-x-1/2 md:top-5 bg-slate-500/40 md:rounded-lg flex items-center space-y-1 md:space-y-3 flex-col p-2"
   >
     <h1 class="text-2xl">Driving</h1>
@@ -7,14 +8,10 @@
       Type "update" to update your current location on the map
     </p>
     <input type="text" class="rounded-md px-1 w-full" v-model="inputText" />
-    <button
-      type="button"
-      class="bg-slate-300 rounded-md py-2 px-6"
-      @click="tryToUpdateLocation()"
-    >
+    <button type="submit" class="bg-slate-300 rounded-md py-2 px-6">
       Send
     </button>
-  </div>
+  </form>
 </template>
 
 <script setup lang="ts">
