6cce954292cc53377b54ec2874ba24826cb30fb2
kaptast <kaptas.tamas@gmail.com>
2022-06-19 15:20:01 +0200

    add OSM attribution

--- bad-ui-map/src/components/Map.vue
+++ bad-ui-map/src/components/Map.vue
@@ -5,6 +5,7 @@
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
         name="OpenStreetMap"
+        attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
       ></l-tile-layer>
       <l-marker :lat-lng="[lat, lon]"></l-marker>
     </l-map>
